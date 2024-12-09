8548f198f9ef0f5fbb93943c75454c899584ab4e
Batukhan0 <batuhankaragol0@gmail.com>
2024-12-09 22:51:49 +0100

    Add a model

--- Controllers/MoviesController.cs
+++ Controllers/MoviesController.cs
@@ -76,7 +76,7 @@ namespace MvcMovie.Controllers
             return View();
         }
 
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
+
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
--- obj/Debug/net8.0/MvcMovie.AssemblyInfo.cs
+++ obj/Debug/net8.0/MvcMovie.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d8c44f8cbf2166b8d87a46d6d115ec657a09019b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0a6a539602d22f1ee5dcefd0dadf9298227773ce")]
 [assembly: System.Reflection.AssemblyProductAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
